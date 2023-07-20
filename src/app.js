558889059dbc6f73b52d4c4b10307abcc9e7881f
claudiasgithub2 <138537469+claudiasgithub2@users.noreply.github.com>
2023-07-20 16:40:00 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+
+const misExcusas = () => {
+  let quien = ["Mi perro", "Mi abuela", "Mi tortuga", "Mi pájaro"];
+  let accion = ["comió", "perdió", "rompió", "aplastó"];
+  let que = ["mi tarea", "las llaves", "el auto"];
+  let cuando = [
+    "antes de clase",
+    "cuando terminé",
+    "durante el almuerzo",
+    "cuando me bañaba"
+  ];
+
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let queIndex = Math.floor(Math.random() * que.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  document.write(
+    quien[quienIndex] +
+      " " +
+      accion[accionIndex] +
+      " " +
+      que[queIndex] +
+      " " +
+      cuando[cuandoIndex]
+  );
 };
+
+misExcusas();
